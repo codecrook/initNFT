7117adbe1792289650a5251567b91b8cb5d1ed46
Deeptiman Mallick <deeptiman_mallick@outlook.com>
2022-02-27 00:07:59 +0530

    ADD: MyEpicNFT contract

---
+++ contracts/MyEpicNFT.sol
@@ -0,0 +1,9 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.2;
+import "hardhat/console.sol";
+
+contract MyEpicNFT {
+    constructor() {
+        console.log("My First NFT Contract!");
+    }
+}
