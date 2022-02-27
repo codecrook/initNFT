e22e8c21f1c538164ba3c376e62f007b9562db23
Deeptiman Mallick <deeptiman_mallick@outlook.com>
2022-02-28 01:28:14 +0530

    ADD: event in contract to be emitted when NFT is minted

--- contracts/MyEpicNFT.sol
+++ contracts/MyEpicNFT.sol
@@ -12,6 +12,7 @@ import {Base64} from "./libraries/Base64.sol";
 contract MyEpicNFT is ERC721URIStorage {
     using Counters for Counters.Counter;
     Counters.Counter private _tokenIds;
+    event NewEpicNFTMinted(address sender, uint256 tokenId);
 
     string baseSvg =
         "<svg xmlns='http://www.w3.org/2000/svg' preserveAspectRatio='xMinYMin meet' viewBox='0 0 350 350'><style>.base { fill: white; font-family: serif; font-size: 24px; }</style><rect width='100%' height='100%' fill='black' /><text x='50%' y='50%' class='base' dominant-baseline='middle' text-anchor='middle'>";
@@ -122,5 +123,7 @@ contract MyEpicNFT is ERC721URIStorage {
             newItemId,
             msg.sender
         );
+
+        emit NewEpicNFTMinted(msg.sender, newItemId);
     }
 }
